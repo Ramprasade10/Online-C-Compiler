3711d03db0d0eea18893a95e8a43ce2003af2564
Ramprasad E Naik <ramprasade10@gmail.com>
2017-06-06 09:34:46 +0530

    Delete SDAD.py

--- SDAD.py
+++
@@ -1,5 +0,0 @@
-
- cmd = ["gcc -o  /home/ramprasad/Desktop/flask/hello.c",""]
-gcc   -o /home/ramprasad/Desktop/flask/hello
-subprocess.call(["gcc","/home/ramprasad/Desktop/flask/hello.c","-o","/home/ramprasad/Desktop/flask/hello"])
-subprocess.call(["/home/ramprasad/Desktop/flask/hello"])
